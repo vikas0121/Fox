36938ae69aeb8265869af49e757c8affa217b6d1
vikas0121 <vikaskumar4793@gmail.com>
2017-11-26 20:45:37 +0530

    adding propertylayer

---
+++ PropertyLayer/InterviewQuestion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyLayer
+{
+    public class InterviewQuestion
+    {
+        public int QuestionId { get; set; }
+        public string Heading { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+        public int CreatedBy { get; set; }
+        public int Language { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public byte IsActive { get; set; }
+        public string Category { get; set; }
+        public string Tag { get; set; }
+    }
+}
